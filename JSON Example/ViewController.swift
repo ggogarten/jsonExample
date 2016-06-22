a1b5630c523392010523e246c14b0437a5a97e9c
George G <ggogarten@gmail.com>
2016-06-22 15:39:28 -0400

    downloading and parsing json

--- JSON Example/ViewController.swift
+++ JSON Example/ViewController.swift
@@ -13,6 +13,35 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let url = NSURL(string: "http://ip-api.com/json")!
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) -> Void in
+            
+            if let urlContent = data {
+                
+                do {
+                    
+                    let jsonResult = try NSJSONSerialization.JSONObjectWithData(urlContent, options: NSJSONReadingOptions.MutableContainers)
+                    
+                    print(jsonResult["city"])
+                    
+                } catch {
+                    
+                    print("JSON serialization failed")
+                    
+                }
+                
+                
+            }
+            
+            
+        }
+        
+        task.resume()
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
